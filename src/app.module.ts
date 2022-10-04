84be61c6d66738400f38bca67ad96c7771c2203b
zzinx58 <q1095568627@163.com>
2022-10-04 12:45:55 +0800

    clean&set project up
    section one

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UserService } from './user/user.service';
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://docker:mongopw@localhost:55000'),
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [UserService],
+})
+export class AppModule {}
